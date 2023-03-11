ab1d20c6cea14c3301c8ad51529c6b1ac7f3d104
David Čadež <david.cadez89@gmail.com>
2023-03-11 16:57:32 +0100

    added CFG file

---
+++ src/compiler/parser/Parser.java
@@ -0,0 +1,57 @@
+/**
+ * @Author: turk
+ * @Description: Sintaksni analizator.
+ */
+
+package compiler.parser;
+
+import static compiler.lexer.TokenType.*;
+import static common.RequireNonNull.requireNonNull;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Optional;
+
+import common.Report;
+import compiler.lexer.Position;
+import compiler.lexer.Symbol;
+import compiler.lexer.TokenType;
+
+public class Parser {
+    /**
+     * Seznam leksikalnih simbolov.
+     */
+    private final List<Symbol> symbols;
+
+    /**
+     * Ciljni tok, kamor izpisujemo produkcije. Če produkcij ne želimo izpisovati,
+     * vrednost opcijske spremenljivke nastavimo na Optional.empty().
+     */
+    private final Optional<PrintStream> productionsOutputStream;
+
+    public Parser(List<Symbol> symbols, Optional<PrintStream> productionsOutputStream) {
+        requireNonNull(symbols, productionsOutputStream);
+        this.symbols = symbols;
+        this.productionsOutputStream = productionsOutputStream;
+    }
+
+    /**
+     * Izvedi sintaksno analizo.
+     */
+    public void parse() {
+        parseSource();
+    }
+
+    private void parseSource() {
+        // TODO: - rekurzivno spuščanje
+    }
+
+    /**
+     * Izpiše produkcijo na izhodni tok.
+     */
+    private void dump(String production) {
+        if (productionsOutputStream.isPresent()) {
+            productionsOutputStream.get().println(production);
+        }
+    }
+}
---
+++ test.pins
@@ -0,0 +1,2 @@
+fun primer(vhod:integer): integer =
+a | b { where fun notranja(vhod:string): integer = { for i = 10, i < 100, i:i+100 }}
